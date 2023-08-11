4203f1ba955b57bc45d7d50fd87b4eb04a78cf14
anton083 <anton.sollman@outlook.com>
2023-08-11 23:05:30 +0200

    Renamed KmerCountVector.jl

--- src/kmer_count_vec.jl
+++ src/kmer_count_vec.jl
@@ -1,11 +1,11 @@
 """
-    KmerCountVector{A, K, T, M <: AbstractMatrix{T}} <: AbstractVector{KmerCount{A, K, T}}
+    AbstractKmerCountVector{A, K, T <: Real, M <: AbstractMatrix{T}} <: AbstractVector{KmerCount{A, K, T}}
 
 A container for K-mer counts, where K-mer counts are stored together as rows or columns in a matrix.
 `A` is the alphabet size, `K` is the K-mer size, `T` is the element type of the counts,
 and `M` is the type of the matrix in which the K-mer counts are stored.
 """
-abstract type AbstractKmerCountVector{A, K, T, M <: AbstractMatrix{T}} <: AbstractVector{KmerCount{A, K, T}} end
+abstract type AbstractKmerCountVector{A, K, T <: Real, M <: AbstractMatrix{T}} <: AbstractVector{KmerCount{A, K, T}} end
 
 struct KmerCountColumns{A, K, T, M} <: AbstractKmerCountVector{A, K, T, M}
     counts::M
@@ -14,11 +14,18 @@ struct KmerCountColumns{A, K, T, M} <: AbstractKmerCountVector{A, K, T, M}
         @assert size(counts, 1) == A^K
         new{A, K, T, M}(counts)
     end
+
+    function KmerCountColumns(kcc::KmerCountRows{A, K, T}) where {A, K, T}
+        KmerCountColumns{A, K, T}(transpose(kcc.counts))
+    end
 end
 
 @inline Base.size(kmer_count::KmerCountColumns) = (size(kmer_count.counts, 2),)
 @inline Base.length(kmer_count::KmerCountColumns) = size(kmer_count.counts, 2)
-@inline Base.getindex(kmer_count::KmerCountColumns{A, K, T}, i::Integer) where {A, K, T} = KmerCount{A, K, T}(view(kmer_count.counts, :, i))
+
+@inline function Base.getindex(kmer_count::KmerCountColumns{A, K, T}, i::Integer) where {A, K, T}
+    KmerCount{A, K, T}(view(kmer_count.counts, :, i))
+end
 
 struct KmerCountRows{A, K, T, M} <: AbstractKmerCountVector{A, K, T, M}
     counts::M
@@ -27,13 +34,18 @@ struct KmerCountRows{A, K, T, M} <: AbstractKmerCountVector{A, K, T, M}
         @assert size(counts, 2) == A^K
         new{A, K, T, M}(counts)
     end
+
+    function KmerCountRows(kcc::KmerCountColumns{A, K, T}) where {A, K, T}
+        KmerCountRows{A, K, T}(transpose(kcc.counts))
+    end
 end
 
 @inline Base.size(kmer_count::KmerCountRows) = (size(kmer_count.counts, 1),)
 @inline Base.length(kmer_count::KmerCountRows) = size(kmer_count.counts, 1)
 @inline Base.getindex(kmer_count::KmerCountRows{A, K, T}, i::Integer) where {A, K, T} = KmerCount{A, K, T}(view(kmer_count.counts, i, :))
 
-Base.transpose(kcc::KmerCountColumns{A, K, T}) where {A, K, T} = KmerCountRows{A, K, T}(transpose(kcc.counts))
-Base.transpose(kcr::KmerCountRows{A, K, T}) where {A, K, T} = KmerCountColumns{A, K, T}(transpose(kcr.counts))
+Base.transpose(kcc::KmerCountColumns) = KmerCountRows(kcc)
+Base.transpose(kcr::KmerCountRows) = KmerCountColumns(kcr)
+
 Base.adjoint(kcc::KmerCountColumns) = transpose(kcc)
 Base.adjoint(kcr::KmerCountRows) = transpose(kcr)
---
+++ test/kmer_count_vec.jl
@@ -0,0 +1,17 @@
+@testset "kmer_count_vec.jl" begin
+
+    @testset "KmerCountVector" begin
+
+        @testset "KmerCountColumns" begin
+            kcc = KmerCountColumns{4, 2, Int}(zeros(Int, (16, 3)))
+            @test length(kcc) == 3
+        end
+
+        @testset "KmerCountRows" begin
+            kcr = KmerCountRows{4, 2, Int}(zeros(Int, (3, 16)))
+            @test length(kcr) == 3
+        end
+
+    end
+
+end
