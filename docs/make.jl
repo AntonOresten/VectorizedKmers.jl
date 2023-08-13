fb2aec2fe0888360b2eb97a8666799c8c6567051
anton083 <anton.sollman@outlook.com>
2023-08-13 12:28:17 +0200

    doc stuff

--- docs/make.jl
+++ docs/make.jl
@@ -4,16 +4,27 @@ using Documenter
 DocMeta.setdocmeta!(VectorizedKmers, :DocTestSetup, :(using VectorizedKmers); recursive=true)
 
 makedocs(;
-    modules=[VectorizedKmers],
-    authors="Anton",
-    repo="https://github.com/anton083/VectorizedKmers.jl/blob/{commit}{path}#{line}",
-    sitename="VectorizedKmers.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        edit_link="main",
-        assets=String[],
+    modules = [VectorizedKmers],
+    authors = "Anton O. Sollman",
+    repo = "https://github.com/anton083/VectorizedKmers.jl/blob/{commit}{path}#{line}",
+    sitename = "VectorizedKmers.jl",
+    doctest = true,
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
     ],
+    checkdocs = :all
+)
+
+deploydocs(;
+    repo = "github.com/anton083/VectorizedKmers.jl",
+    push_preview = true,
+    branch = "gh-pages",
+    devbranch = "dev",
+    deps = nothing,
+    make = nothing
 )
