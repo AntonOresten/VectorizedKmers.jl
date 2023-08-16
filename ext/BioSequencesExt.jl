ee9cfdbb4a80c428932e7ccd5b94fd9bd5a5949f
anton083 <anton.sollman@outlook.com>
2023-08-16 23:54:23 +0200

    more methods

--- ext/BioSequencesExt.jl
+++ ext/BioSequencesExt.jl
@@ -4,6 +4,28 @@ using VectorizedKmers, BioSequences
 
 function VectorizedKmers.count_kmers!(
     kmer_count::KmerCount{4, K, T},
+    seq::LongDNA{2};
+    reset::Bool = true,
+) where {K, T}
+    counts = kmer_count.counts
+    reset && reset!(kmer_count)
+    len = length(seq)
+    mask = UInt(1) << 2K - 1
+    kmer = UInt(0)
+    i = 0
+    @inbounds for data_int in seq.data
+        for j in 0:2:63
+            i += 1
+            i > len && break
+            kmer = ((kmer << 2) & mask) | ((data_int >> j) & 0b11)
+            counts[kmer + 1] += K <= i
+        end
+    end
+    kmer_count
+end
+
+function VectorizedKmers.count_kmers!(
+    kmer_count::KmerCount{4, K, T},
     seq::LongDNA{4};
     reset::Bool = true,
 ) where {K, T}
@@ -17,7 +39,7 @@ function VectorizedKmers.count_kmers!(
         for j in 0:4:63 # could maybe do some SIMD shit on middle K-mers
             i += 1
             i > len && break # only necessary for the last element in seq.data
-            kmer = kmer << 2 & mask + trailing_zeros(data_int >> j) & 0b11
+            kmer = ((kmer << 2) & mask) | (trailing_zeros(data_int >> j) & 0b11)
             counts[kmer + 1] += K <= i
         end
     end
@@ -25,13 +47,17 @@ function VectorizedKmers.count_kmers!(
 end
 
 function VectorizedKmers.count_kmers(
-    ::Type{KmerCount{4, K, T}},
-    seq::LongDNA{4};
+    KC::Type{KmerCount{4, K, T}},
+    seq::LongDNA;
     zeros_func::Function = zeros,
 ) where {K, T}
-    kmer_count = KmerCount{4, K, T}(zeros_func)
+    kmer_count = KC(zeros_func)
     count_kmers!(kmer_count, seq, reset=false)
     kmer_count
+end
+
+function VectorizedKmers.count_kmers(seq::LongDNA, K::Integer, T::DataType=UInt32)
+    count_kmers(KmerCount{4, K, T}, seq)
 end
 
 end
