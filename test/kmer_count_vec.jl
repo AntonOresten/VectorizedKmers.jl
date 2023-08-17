91796e17547475ab1826b35a3629bb33ef726316
anton083 <anton.sollman@outlook.com>
2023-08-17 13:14:49 +0200

    tests

--- test/kmer_count_vec.jl
+++ test/kmer_count_vec.jl
@@ -4,12 +4,20 @@
 
         @testset "KmerCountColumns" begin
             kcc = KmerCountColumns{4, 2}(zeros(Int, (16, 3)))
+            @test size(kcc) == (3,)
             @test length(kcc) == 3
+            @test kcc[1] == KmerCount{4, 2}()
+
+            @test transpose(kcc) == kcc'
         end
 
         @testset "KmerCountRows" begin
             kcr = KmerCountRows{4, 2}(zeros(Int, (3, 16)))
+            @test size(kcr) == (3,)
             @test length(kcr) == 3
+            @test kcr[1] == KmerCount{4, 2}()
+
+            @test transpose(kcr) == kcr'
         end
 
     end
