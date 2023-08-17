0947d093e02f9b24fb5e8c323b2e32c40bc732fd
anton083 <anton.sollman@outlook.com>
2023-08-17 13:00:36 +0200

    Update BioSequencesExt.jl

--- test/ext/BioSequencesExt.jl
+++ test/ext/BioSequencesExt.jl
@@ -1,3 +1,18 @@
+using BioSequences
+
 @testset "BioSequencesExt.jl" begin
+
+    @testset "count_kmers!" begin
+
+        @test count_kmers(dna"ACGT", 1).counts == [1, 1, 1, 1]
+        @test count_kmers(dna"ACGT", 1) == count_kmers(LongDNA{2}(dna"ACGT"), 1)
+
+        kc = KmerCount{4, 1, Int}()
+        @test count_kmers!(kc, dna"ACGT").counts == [1, 1, 1, 1]
+
+        kc = KmerCount{4, 2, Int}()
+        @test count_kmers!(kc, dna"ACGT").counts == [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+
+    end
     
 end
