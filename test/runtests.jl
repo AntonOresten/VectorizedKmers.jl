b4fadbde38e277feb736449e32746684c2dd49c4
anton083 <anton.sollman@outlook.com>
2023-08-21 18:31:48 +0200

    Renamed src files

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Test
 
 @testset "VectorizedKmers.jl" begin
 
-    include("kmer_count.jl")
-    include("kmer_count_vec.jl")
+    include("kmer_count_vector.jl")
+    include("kmer_count_matrix.jl")
 
     @testset "extensions" begin
         include("ext/BioSeqCUDAExt.jl")
