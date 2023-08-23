012a9e87b0ebf3642983c12bcb34e17e0dc03726
anton083 <anton.sollman@outlook.com>
2023-08-23 22:56:33 +0200

    Update counting.jl

--- test/counting.jl
+++ test/counting.jl
@@ -9,6 +9,9 @@
     @testset "count_kmers" begin
         @test count_kmers(Ref([2, 0, 3, 3, 0, 1, 0]), 4, 1) == KmerCountVector{4, 1}([3, 1, 1, 2])
         @test count_kmers([Ref([2, 0, 3, 3, 0, 1, 0])], 4, 1) == KmerCountColumns{4, 1}(reshape([3, 1, 1, 2], :, 1))
+        @test sum(count_kmers(Ref([2, 0, 3, 3, 0, 1, 0]), 4, 2).counts) == 6
     end
+
+    @test_throws ErrorException VectorizedKmers.alphabet_size(DataType)
 
 end
