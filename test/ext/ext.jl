2fea9a4a21a3c0e0e55bed20fd42e0b8f7faf7de
anton083 <anton.sollman@outlook.com>
2023-08-25 00:55:46 +0200

    no longer testing CUDA exts

--- test/ext/ext.jl
+++ test/ext/ext.jl
@@ -1,7 +1,7 @@
 @testset "extensions" begin
 
-    include("BioSeqCUDAExt.jl")
+    #include("BioSeqCUDAExt.jl")
     include("BioSequencesExt.jl")
-    include("CUDAExt.jl")
+    #include("CUDAExt.jl")
 
 end
