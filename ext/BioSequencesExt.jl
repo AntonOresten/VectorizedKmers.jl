0e71395ab908dd9a0f9a110c2a631eaa7d0e14d4
anton083 <anton.sollman@outlook.com>
2023-10-04 22:28:11 +0200

    BitVector "counting"

--- ext/BioSequencesExt.jl
+++ ext/BioSequencesExt.jl
@@ -58,4 +58,32 @@ end
     return kmer_vector
 end
 
+@inline function VectorizedKmers.count_kmers!(
+    kmer_vector::KmerVector{4, k, Bool, BitVector},
+    sequence::LongNucOrView{4};
+    reset::Bool = true,
+) where k
+    chunks = kmer_vector.values.chunks
+    reset && fill!(chunks, zero(UInt))
+    len = length(sequence)
+    len < k && return kmer_vector
+    mask = UInt(4^k - 1)
+    kmer = UInt(0)
+    start, stop = sequence isa LongSubSeq ? (sequence.part.start, sequence.part.stop) : (1, len)
+    data_start, data_stop = (start - 1) ÷ 16 + 1, (stop - 1) ÷ 16 + 1
+    first_count_index = k + start - 1
+    i = 16 * (data_start - 1)
+    @inbounds for data_int in @view sequence.data[data_start:data_stop]
+        for j in 0:4:63
+            i += 1
+            i > stop && break
+            kmer = ((kmer << 2) & mask) | (trailing_zeros(data_int >> j) & 0b11)
+            chunk_index = kmer >> 6 + one(UInt)
+            index_in_chunk = kmer & 0x3f
+            first_count_index <= i && (chunks[chunk_index] ⊻= one(UInt) << index_in_chunk)
+        end
+    end
+    return kmer_vector
+end
+
 end
