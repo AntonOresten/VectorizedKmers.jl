216af323efe2538c62df016a49a3d87ac83baf6c
anton083 <anton.sollman@outlook.com>
2023-10-15 21:57:03 +0200

    Changed the meaning `D`
    1 is now for column-vectors and 2 is now for row-vectors.

--- src/vectors.jl
+++ src/vectors.jl
@@ -3,10 +3,7 @@
 
 A container for k-mer vectors, where the values are stored together as columns or rows in a matrix.
 
-!!! note
-    It might be more efficient to store the values as columns in a matrix, since the elements in a column are contiguous in memory.
-
-`D` is the dimension of the matrix in which the vectors are next to each other (1 for rows, 2 for columns).
+`D` is the dimension of the vectors (i.e. `1` for column-vectors, `2` for row-vectors).
 `S` is the alphabet size,
 `k` is the k-mer length,
 and `T` is the element type of the underlying `values` field,
@@ -17,23 +14,23 @@ struct KmerVectors{D, S, k, T, M} <: AbstractKmerMatrix{S, k, T, M}
 
     function KmerVectors{D, S, k}(values::M) where {D, S, k, T, M <: AbstractMatrix{T}}
         @assert D isa Integer && 1 <= D <= 2 "Type parameter `D` must be an integer between 1 and 2."
-        @assert size(values, 3-D) == S^k
+        @assert size(values, D) == S^k
         return new{D, S, k, T, M}(values)
     end
 
     function KmerVectors{D, S, k}(n::Integer; T::Type{<:Real}=Int, zeros::Function=zeros) where {D, S, k}
-        dims = (D == 1) ? (n, S^k) : (S^k, n)
+        dims = (D == 1) ? (S^k, n) : (n, S^k)
         return KmerVectors{D, S, k}(zeros(T, dims))
     end
 end
 
-const KmerRows = KmerVectors{1}
-const KmerColumns = KmerVectors{2}
+const KmerColumns = KmerVectors{1}
+const KmerRows = KmerVectors{2}
 
 # used to distinguish between KmerColumns{4, 2}(4) and KmerRows{4, 1}(16)
 Base.hash(kvs::KmerVectors{D, S}, h::UInt) where {D, S} = hash(kvs.values, h ⊻ S ⊻ D)
 
-@inline Base.length(kvs::KmerVectors{D}) where D = size(kvs, D)
+@inline Base.length(kvs::KmerVectors{D}) where D = size(kvs, 3-D)
 
 @inline function Base.getindex(krs::KmerRows{S, k}, i) where {S, k}
     return KmerVector{S, k}(view(krs.values, i, :))
@@ -44,5 +41,5 @@ end
 end
 
 # could make a proper type for this that isn't just a generator
-@inline _eachvec_iter(kvs::KmerVectors{D}) where D = (D == 1) ? eachrow(kvs.values) : eachcol(kvs.values)
+@inline _eachvec_iter(kvs::KmerVectors{D}) where D = (D == 1) ? eachcol(kvs.values) : eachrow(kvs.values)
 @inline eachvec(kvs::KmerVectors{D, S, k}) where {D, S, k} = (KmerVector{S, k}(v) for v in _eachvec_iter(kvs))
