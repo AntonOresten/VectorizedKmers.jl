0df08dd47bc07796fc34b2c7275cfe8df9d1a200
anton083 <anton.oresten42@gmail.com>
2024-03-06 12:21:57 +0100

    Update counting.jl

--- src/counting.jl
+++ src/counting.jl
@@ -1,18 +1,18 @@
 """
-    count_kmers!(kmer_array, sequence; reset=true)
+    count_kmers!(kmer_array, seq; reset=true)
 """
 function count_kmers! end
 
-# primarily meant to be used for testing
+# primarily meant for testing purposes
 function count_kmers!(
     kmer_array::KmerArray{N, K}, sequence::Vector{Int};
     reset::Bool = true,
 ) where {N, K}
-    @assert 0 <= maximum(sequence) < N
+    0 <= maximum(seq) < N || throw(ArgumentError("seq contains elements outside the range 0:N-1"))
     reset && zeros!(kmer_array)
     mask = N^K
     kmer = zero(Int)
-    for (i, m) in enumerate(sequence)
+    for (i, m) in enumerate(seq)
         kmer = kmer * N % mask + m
         K <= i && (kmer_array[kmer] += 1)
     end
@@ -20,17 +20,13 @@ function count_kmers!(
 end
 
 """
-    count_kmers(sequence, N, K, T=Int, zeros=zeros)
+    count_kmers(seq, N, K, T=Int, zeros=zeros)
 """
 function count_kmers end
 
-function count_kmers(sequence, N::Integer, K::Integer, T::Type{<:Real}=Int, zeros=zeros)
-    return count_kmers!(KmerArray(N, K, T, zeros), sequence; reset=false)
-end
+count_kmers(seq, N::Integer, K::Integer, T::Type{<:Real}=Int, zeros=zeros) = count_kmers!(KmerArray(N, K, T, zeros), seq; reset=false)
 
 alphabet_size(T::Type) = error("$(T) does not have a defined alphabet size. Please define `alphabet_size(::Type{<:$(T)})` or insert the alphabet size as a second argument in the `count_kmers` function call.")
 alphabet_size(::T) where T = alphabet_size(T)
 
-function count_kmers(sequence, K::Integer, T::Type{<:Real}=Int, zeros=zeros)
-    return count_kmers(sequence, alphabet_size(sequence), K, T, zeros)
-end
+count_kmers(seq, K::Integer, T::Type{<:Real}=Int, zeros=zeros) = count_kmers(seq, alphabet_size(seq), K, T, zeros)
