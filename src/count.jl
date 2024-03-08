e25961bb5c4e047fcd8738ed483071a6b408816b
anton083 <anton.oresten42@gmail.com>
2024-03-08 13:28:11 +0100

    Update count.jl

--- src/count.jl
+++ src/count.jl
@@ -1,17 +1,14 @@
 """
     count_kmers!(kmer_array, sequence; reset=true)
-"""
-function count_kmers! end
 
-"""
-    count_kmers!(kmer_array, sequence; reset=true)
+Requires method `axis_index(::KmerArray{N}, ::eltype(sequence)) where N` to be defined
 """
 function count_kmers!(kmer_array::KmerArray{N, K}, sequence; reset::Bool = true) where {N, K}
     reset && zeros!(kmer_array)
     mask = N^K
     kmer = zero(UInt)
     for (i, m) in enumerate(sequence)
-        kmer = kmer * N % mask + axis_index(kmer_array, m)
+        kmer = (kmer * N + axis_index(kmer_array, m)) % mask
         kmer_array[kmer] += K <= i
     end
     return kmer_array
@@ -20,11 +17,6 @@ end
 """
     count_kmers(sequence, K, T=Int, zeros=zeros; N=default_alphabet_size(eltype(sequence)))
 """
-function count_kmers end
-
-function count_kmers(
-    sequence, K::Integer, T::Type{<:Real}=Int, zeros=zeros;
-    N::Integer = default_alphabet_size(eltype(sequence)),
-)
+function count_kmers(sequence, K::Integer, T::Type{<:Real}=Int, zeros=zeros; N::Integer = default_alphabet_size(eltype(sequence)))
     return count_kmers!(KmerArray(N, K, T, zeros), sequence; reset=false)
 end
